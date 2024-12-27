baf03f3d0fa5c202d489ca5ccfba77be9a0a8120
ISOURA <84765989+isoura4@users.noreply.github.com>
2024-12-27 20:02:33 +0100

    Update startCounter.js

--- commands/startCounter.js
+++ commands/startCounter.js
@@ -4,16 +4,18 @@ const { getCounter, setCounter } = require('../utils/counter');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('startcounter')
-        .setDescription('Démarrer le jeu de compteur dans un salon spécifique.')
+        .setDescription('Démarre le jeu de compteur dans le salon spécifié.')
         .addChannelOption(option =>
             option.setName('channel')
-                .setDescription('Le salon où démarrer le jeu.')
-                .setRequired(true)
-                .addChannelTypes(0)), // 0 for GUILD_TEXT
+                .setDescription('Le salon où démarrer le jeu de compteur.')
+                .setRequired(true)),
     async execute(interaction) {
         const channel = interaction.options.getChannel('channel');
-        setCounter(0, null);
+        const guildId = interaction.guild.id;
 
-        await interaction.reply(`Le jeu de compteur a démarré dans le salon <#${channel.id}>. Le compteur est à 0.`);
-    },
+        // Initialiser le compteur pour le salon spécifié
+        setCounter(guildId, 0, null, channel.id);
+
+        await interaction.reply(`Le jeu de compteur a été démarré dans le salon <#${channel.id}>.`);
+    }
 };
